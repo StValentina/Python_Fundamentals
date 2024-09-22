5883d04c6cade1c929e08f3a54a45933d2b6dd94
StValentina <valentina.st999@gmail.com>
2024-09-22 12:17:16 +0300

    Update empty_diamand_with_input.py

--- pattern drawing/empty_diamand_with_input.py
+++ pattern drawing/empty_diamand_with_input.py
@@ -1,14 +1,12 @@
-# Въвеждане на размера от потребителя
-size = int(input("Въведете размер на диаманта (нечетно число): "))
+n = int(input("Въведете размер на диаманта (нечетно число): "))
 
-# Проверка дали въведеното число е нечетно
-if size % 2 == 0:
-    size += 1
+if n % 2 == 0:
+    n += 1
 
-for row in range(size):
-    for col in range(size):
-        if (row + col == size // 2) or (col - row == size // 2) or \
-           (row - col == size // 2) or (row + col == size + size // 2 - 1):
+for row in range(n):
+    for col in range(n):
+        if (row + col == n // 2) or (col - row == n // 2) or \
+           (row - col == n // 2) or (row + col == n + n // 2 - 1):
             print('*', end=' ')
         else:
             print(' ', end=' ')
